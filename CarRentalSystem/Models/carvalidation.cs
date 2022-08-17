9428e6aa60d9c8d140a6f2b9ede8e04a93bc3257
Mark Josmil Degamo <MDegamo01@lear.com>
2022-08-17 13:54:52 +0800

    a

---
+++ CarRentalSystem/Models/carvalidation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CarRentalSystem.Models
+{
+    [MetadataType(typeof(carregMetadata))]
+    public partial class carreg
+    {
+        public class carregMetadata
+        {
+            [DisplayName("Car no. ")]
+            public string carno { get; set; }
+            [DisplayName("Brand ")]
+            public string make { get; set; }
+            [DisplayName("Model ")]
+            public string model { get; set; }
+            [DisplayName("Availability ")]
+            public string available { get; set; }    
+        }
+    }
+}
---
+++ CarRentalSystem/Models/customervalidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CarRentalSystem.Models
+{
+    [MetadataType(typeof(customerMetaData))]
+    public partial class customer
+    {
+        public class customerMetaData
+        {
+            [DisplayName("Customer Name ")]
+            public string custname { get; set; }
+            [DisplayName("Customer Address ")]
+            public string address { get; set; }
+            [DisplayName("Customer Mobile# ")]
+            public string mobile { get; set; }
+        }
+    }
+}
